492c668db18632c3727cb614205abeecf3eecfd6
waomi69 <ikuldarev@mail.ru>
2023-12-14 22:45:33 +0500

    add regex checks

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -19,13 +19,29 @@ export default () => {
   };
 
   var compartment_name = "Безымянный";
+  
 
   const onEnterPressed = (event) => {
-    if (event.key === 'Enter') {
+    const hasSpecialCharacters = /[!@#$%^&*(),.?":{}|<>]/.test(event.target.value)
+    if (event.key === 'Enter' && event.target.value !== '' && !hasSpecialCharacters) 
+    {
       compartment_name = event.target.value;
       console.log('Введенное значение:', compartment_name);
-    }
+    } 
+
   };
+
+  const onCheckSymbolsandNum = (event) => {
+    const hasSpecialCharacters = /[!@#$%^/\\\-=+\_&*(),.?":{}|<>]/.test(event.target.value)
+    const hasNumbers = /[0-9]/.test(event.target.value)
+    if(hasSpecialCharacters || hasNumbers) 
+    {
+      var user_input = event.target.value
+      document.getElementById("comparment_input_text").value = user_input.slice(0,-1)
+    } 
+
+  };
+
 
   return (
     <aside>
@@ -36,7 +52,7 @@ export default () => {
       <div className="description">Напишите название компартмента.</div>
       <div>
         <label htmlFor="text">Имя:</label>
-        <input id="comparment_input_text"  onKeyDown={(event) => onEnterPressed(event)}/> 
+        <input id="comparment_input_text"  onKeyDown={(event) => onEnterPressed(event)} maxlength="20" onChange={(event) => onCheckSymbolsandNum(event)}/> 
         {/* TODO: нужна реакция на Enter. */}
       </div>
       <div className="dndnode compartmentNode" onDragStart={(event) => onDragStart(event, 'compartmentNode', compartment_name)} draggable>
